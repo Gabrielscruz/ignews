ecd981d5f1b9f5d7c25b11a84bf8a06a410fb204
Gabrielscruz <54606517+Gabrielscruz@users.noreply.github.com>
2022-05-16 00:23:18 -0300

    gravando_pagamento

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -25,6 +26,7 @@ const relevantEvents = new Set([
     'checkout.session.completed'
 ]);
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === "POST") {
         const buf = await buffer(req);
@@ -37,8 +39,23 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         } catch (err) {
             return res.status(400).send(`Webhook error: ${err.message}`);
         }
-        if(relevantEvents.has(event.type)) {
-            console.log('Evento recebido', event);
+        const { type } = event
+        if(relevantEvents.has(type)) {
+            try {
+                switch(type) {
+                    case 'checkout.session.completed':
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session;
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString()
+                        )
+                        break;
+                    default:
+                        throw new Error('Unhandled event.')
+                }
+            } catch (err) {
+                return res.json({error: 'webhook handler failed.'})
+            }
         }
         res.json({ received: true });
     } else {
